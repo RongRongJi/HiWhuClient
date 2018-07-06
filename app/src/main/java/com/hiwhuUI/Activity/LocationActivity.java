277308ca28d3da84732610bf8881e8acd3cb3245
PHBPLRS <846223176@163.com>
2018-07-06 21:02:15 +0800

    主页面提交

---
+++ app/src/main/java/com/hiwhuUI/Activity/LocationActivity.java
@@ -0,0 +1,14 @@
+package com.hiwhuUI.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hiwhu.hiwhuclient.R;
+
+public class LocationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
+}
