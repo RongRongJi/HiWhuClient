234267fb58934c038436e9278f0244663477e664
PHBPLRS <846223176@163.com>
2018-07-07 04:54:12 +0800

    主页面完成

---
+++ app/src/main/java/com/hiwhuUI/Activity/util/Card.java
@@ -0,0 +1,38 @@
+package com.hiwhuUI.Activity.util;
+
+public class Card {
+    private String title;
+    private String img_id;
+    private String time;
+    private String location;
+    private boolean signup;
+    //private boolean star;
+
+    public Card(String title,String img_id,String time,String location,boolean signup){
+        this.title = title;
+        this.img_id =img_id;
+        this.time = time;
+        this.location = location;
+        this.signup = signup;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImg_id() {
+        return img_id;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public boolean isSignup() {
+        return signup;
+    }
+}
