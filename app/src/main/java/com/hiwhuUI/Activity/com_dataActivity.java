966bc2e23481f4104db54203d0adb4fb04348b20
PHBPLRS <846223176@163.com>
2018-07-10 03:30:53 +0800

    添加个人页跳转

--- app/src/main/java/com/hiwhuUI/Activity/com_dataActivity.java
+++ app/src/main/java/com/hiwhuUI/Activity/com_dataActivity.java
@@ -25,6 +25,9 @@ public class com_dataActivity extends AppCompatActivity {
         ImageView head = (ImageView) findViewById(R.id.com_h_head);
         Button button1 = (Button) findViewById(R.id.btn_com_data_cancel) ;
         RelativeLayout relativeLayout1 = (RelativeLayout) findViewById(R.id.com_data_describe);
+        RelativeLayout relativeLayout2 = (RelativeLayout) findViewById(R.id.com_data_unchecked);
+        //RelativeLayout relativeLayout3 = (RelativeLayout) findViewById(R.id.com_data_review);
+        RelativeLayout relativeLayout4 = (RelativeLayout) findViewById(R.id.com_data_history);
         //设置背景磨砂效果
         Glide.with(this).load(R.drawable.stu_data_back)
                 .bitmapTransform(new BlurTransformation(this, 25), new CenterCrop(this))
@@ -39,7 +42,6 @@ public class com_dataActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(com_dataActivity.this,com_describeActivity.class);
                 startActivity(intent);
-
             }
         });
         //取消按钮
@@ -47,6 +49,26 @@ public class com_dataActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new  Intent(com_dataActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        //审核报名人员
+        relativeLayout2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(com_dataActivity.this, ListActivity.class);
+                intent.putExtra("id",5);
+                startActivity(intent);
+            }
+        });
+        //活动回顾
+        //发布历史
+        relativeLayout4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(com_dataActivity.this, ListActivity.class);
+                intent.putExtra("id",6);
                 startActivity(intent);
             }
         });
