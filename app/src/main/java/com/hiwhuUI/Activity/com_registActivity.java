368199e074d40fd5b2200f7080c4e17abf27796b
iss2016302580133 <987761617@qq.com>
2018-07-19 19:40:52 +0800

    确认密码成功

--- app/src/main/java/com/hiwhuUI/Activity/com_registActivity.java
+++ app/src/main/java/com/hiwhuUI/Activity/com_registActivity.java
@@ -27,6 +27,13 @@ public class com_registActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_com_regist);
 
+        final EditText sponsorName = (EditText)findViewById(R.id.name);
+        final EditText phoneNum = (EditText)findViewById(R.id.tel);
+        final EditText password = (EditText)findViewById(R.id.pwd);
+        final EditText introduction = (EditText)findViewById(R.id.description);
+        final EditText Repassword = (EditText)findViewById(R.id.confirm_pwd) ;
+        Button registButton = (Button)findViewById(R.id.btn_com_regist);
+
         //更改标题栏
         TextView title = (TextView)findViewById(R.id.text_title) ;
         title.setText("注册");
@@ -47,58 +54,68 @@ public class com_registActivity extends AppCompatActivity {
             actionBar.hide();
         }
 
-        final EditText sponsorName = (EditText)findViewById(R.id.name);
-        final EditText phoneNum = (EditText)findViewById(R.id.tel);
-        final EditText passwrod = (EditText)findViewById(R.id.pwd);
-        final EditText introduction = (EditText)findViewById(R.id.description);
-        Button registButton = (Button)findViewById(R.id.btn_com_regist);
+
 
         registButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String url = staticData.getUrl()+"/AddSponsorServlet";
                 String name = sponsorName.getText().toString();
                 String tel = phoneNum.getText().toString();
-                String pwd = passwrod.getText().toString();
+                String pwd = password.getText().toString();
                 String desc = introduction.getText().toString();
-                try{
-                    name = java.net.URLEncoder.encode(name,"UTF-8");
-                    desc = java.net.URLEncoder.encode(desc,"UTF-8");
-                    url = url+"?sponsorName="+name+"&phoneNum="+ tel +"&password="+pwd+
-                    "&introduction="+desc;
-                    Log.e("url----",url);
-                }catch(Exception e){
-                    e.printStackTrace();
+                String rpwd = Repassword.getText().toString();
+                if(name.length() == 0 && tel.length() != 0 && desc.length() != 0 && pwd.length() != 0 && pwd.equals(rpwd)
+                        ||name.length() != 0 && tel.length() == 0 && desc.length() != 0 && pwd.length() != 0 && pwd.equals(rpwd)
+                        ||name.length() != 0 && tel.length() != 0 && desc.length() == 0 && pwd.length() != 0 && pwd.equals(rpwd)
+                        ||name.length() != 0 && tel.length() != 0 && desc.length() != 0 && pwd.length() == 0 && pwd.equals(rpwd)) {
+                    Toast.makeText(com_registActivity.this,"请检查是否还有未填写信息！",Toast.LENGTH_LONG).show();
+                    return;
                 }
-                HttpUtil.sendOkHttpRequest(url,new okhttp3.Callback(){
-                    @Override
-                    public void onResponse(Call call, Response response) throws IOException {
-                        String s = response.body().string();
-                        Log.e("return---",s);
-                        if(s.equals("succeed\r\n")){
-                            Jump(true);
-                        }else{
-                            Jump(false);
+                else if(name.length() != 0 && tel.length() != 0 && desc.length() != 0 && pwd.length() != 0 && !pwd.equals(rpwd)){
+                    Toast.makeText(com_registActivity.this,"密码输入不一致！",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else{
+                    String url = staticData.getUrl()+"/AddSponsorServlet";
+                    try{
+                        name = java.net.URLEncoder.encode(name,"UTF-8");
+                        desc = java.net.URLEncoder.encode(desc,"UTF-8");
+                        url = url+"?sponsorName="+name+"&phoneNum="+ tel +"&password="+pwd+
+                                "&introduction="+desc;
+                        Log.e("url----",url);
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                    HttpUtil.sendOkHttpRequest(url,new okhttp3.Callback(){
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            String s = response.body().string();
+                            Log.e("return---",s);
+                            if(s.contains("succeed")){
+                                Jump(1);
+                            }else{
+                                Jump(3);
+                            }
                         }
-                    }
 
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                    }
-                });
-
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                        }
+                    });
+                }
             }
         });
     }
 
-    public void Jump(final boolean flag){
+    public void Jump(final int flag){
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                if(flag){
+                if(flag == 1){
                     Toast.makeText(com_registActivity.this,"您已经成功注册！",Toast.LENGTH_LONG).show();
                     finish();
-                }else{
+                }
+                else{
                     Toast.makeText(com_registActivity.this,"注册失败！",Toast.LENGTH_LONG).show();
                 }
             }
